b6cde72948b5ed7ae41c9b90c2ba2820b201dc48
Majid Abdollahi <majid96abdollahi@gmail.com>
2021-12-21 03:43:00 -0800

    q2 probably completed

--- spark_sort.py
+++ spark_sort.py
@@ -1,70 +1,7 @@
-import random
 import sys
-import time
 from array import array
 
 from pyspark.sql import SparkSession
-
-
-def execute_merge_sort(generated_list):
-    start_time = time.time()
-    sorted_list = merge_sort(generated_list)
-    elapsed = time.time() - start_time
-    print('Simple merge sort: %f sec' % elapsed)
-    return sorted_list
-
-
-def generate_list(length):
-    N = length
-    generated_list = [random.random() for num in range(N)]
-    return generated_list
-
-
-def merging(left_side, right_side):
-    result = []
-    i = j = 0
-    while i < len(left_side) and j < len(right_side):
-        if left_side[i] <= right_side[j]:
-            result.append(left_side[i])
-            i += 1
-        else:
-            result.append(right_side[j])
-            j += 1
-    if i == len(left_side):
-        result.extend(right_side[j:])
-    else:
-        result.extend(left_side[i:])
-    return result
-
-
-def merge_sort(generated_list):
-    print(generated_list)
-    if len(generated_list) <= 1:
-        return generated_list
-    middle_value = len(generated_list) // 2
-    sorted_list = merging(merge_sort(generated_list[:middle_value]), merge_sort(generated_list[middle_value:]))
-    return sorted_list
-
-
-def is_sorted(num_array):
-    for i in range(1, len(num_array)):
-        if num_array[i] < num_array[i - 1]:
-            return False
-    return True
-
-
-def merge(x, y):
-    if isinstance(x, list):
-        left_side = x
-    else:
-        left_side = [x]
-
-    if isinstance(y, list):
-        right_side = y
-    else:
-        right_side = [y]
-
-    return merging(left_side, right_side)
 
 
 def print_in_file(arr, file_name):
